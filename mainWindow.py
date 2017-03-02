487c56611edd08141942e5f0da7cad2557e2c0f5
Vasyl Novosad <novosadvasiliy@gmail.com>
2017-03-03 01:40:45 +0200

    added image verification (OpenCV)

--- mainWindow.py
+++ mainWindow.py
@@ -6,6 +6,7 @@ import selfcheckout_ui
 import scanedform_ui
 import requests
 import os
+import cv2
 
 style = os.path.join(os.path.dirname(__file__), 'style.css')
 
@@ -15,9 +16,41 @@ class MyDialog(QWidget, scanedform_ui.Ui_Dialog):
         self.scannedGood = None
         self.setupUi(self)
         self.cancelPushButton.clicked.connect(self.closeDialog)
+        self.verifyPushButton.clicked.connect(self.verifyGood)
+        self.cancelPushButton2.clicked.connect(self.closeDialog)
+        self.verifyPushButton2.clicked.connect(self.verifyGood)
+        self.cancelPushButton3.clicked.connect(self.closeDialog)
 
     def closeDialog(self):
         self.hide()
+
+    def verifyGood(self):
+        if self.scannedGood == None:
+            return
+        barcode = self.scannedGood['barcode']
+        if self.verifiedGood(barcode):
+            self.buttonsStackedWidget.setCurrentIndex(2)
+            self.parent().goods.append(self.scannedGood)
+            self.parent().shopingCartListWidget.addItem(self.scannedGood['name'])
+
+        else:
+            self.buttonsStackedWidget.setCurrentIndex(1)
+
+    def verifiedGood(self, barcode):
+        # TODO: add OpenCV frame check
+        return True
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cap.release()
+        cv2.imwrite('images/tmp_frame.jpg', frame)
+        url = 'http://40.68.188.63:10002/upload2/' + str(barcode)
+        files = {'file': open('images/tmp_frame.jpg', 'rb')}
+        r = requests.post(url, files=files)
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+
 
 class MyMainWindow(QMainWindow, selfcheckout_ui.Ui_MainWindow):
     def __init__(self):
@@ -26,67 +59,76 @@ class MyMainWindow(QMainWindow, selfcheckout_ui.Ui_MainWindow):
         self.scannedGood = None
         self.discountGoods = [
             {
-                'image': '',
+                'image': 'images/991.jpg',
                 'name': 'Bread',
-                'description': "Calories, kcal: 242\nProteins: 8.1\nFats: 1.0\nCarbohydrates: 48.8",
+                'description': "Calories, kcal: 242 Proteins: 8.1 Fats: 1.0 Carbohydrates: 48.8",
                 'expiration': '2 days',
-                'price': 1.29
+                'price': 1.29,
+                'barcode': '1111'
             },
             {
-                'image': '',
+                'image': 'images/9402-bananas.jpg',
                 'name': 'Banana',
                 'description': 'Banana',
                 'expiration': '2 weeks',
-                'price': 2.19
+                'price': 2.19,
+                'barcode': '2222'
             },
             {
-                'image': '',
+                'image': 'images/salami.jpg',
                 'name': 'Salami',
-                'description': "Proteins: 13 g\nFats: 57 g\nCarbohydrates: 0.2",
+                'description': "Proteins: 13g Fats: 57g Carbohydrates: 0.2",
                 'expiration': '3 months',
-                'price': 10.69
+                'price': 10.69,
+                'barcode': '3333'
             },
             {
-                'image': '',
-                'name': 'French bread',
-                'description': "Calories, kcal: 242\nProteins: 8.1\nFats: 1.0\nCarbohydrates: 48.8",
+                'image': 'images/FrenchBaguette.jpg',
+                'name': 'French Baguette',
+                'description': "Calories, kcal: 242 Proteins: 8.1 Fats: 1.0 Carbohydrates: 48.8",
                 'expiration': '2 days',
-                'price': 1.29
+                'price': 1.29,
+                'barcode': '4444'
             },
             {
-                'image': '',
-                'name': 'Fish conserve',
-                'description': 'Fish conserve',
+                'image': 'images/img-fiche-silver-sardine-huile-vegetale.png',
+                'name': 'Fish Sardine',
+                'description': 'Fish Sardine',
                 'expiration': '1 year',
-                'price': 3.79
+                'price': 3.79,
+                'barcode': '5555'
             },
             {
-                'image': '',
+                'image': 'images/Lemon_easter_biscuits_hero_1d74c01d.jpg',
                 'name': 'Lemon',
-                'description': "Proteins - 0.9 g\nFats - 0.1 g\nCarbohydrates - 4.9 g (including mono - and bioses – 3 g)\nFood fibers (cellulose) – 1.3 g\nPectin - 0.5 g\nOrganic acids - 5.7 g\nAshes - 0.5 g",
+                'description': "Proteins - 0.9g Fats - 0.1g Carbohydrates - 4.9g (including mono - and bioses - 3g) Food fibers (cellulose) - 1.3g Pectin - 0.5g Organic acids - 5.7g Ashes - 0.5g",
                 'expiration': '3 months',
-                'price': 2.89
+                'price': 2.89,
+                'barcode': '6666'
             },
             {
-                'image': '',
+                'image': 'images/liquor-bottle-IARzFh-clipart.jpg',
                 'name': 'Alcohol',
-                'description': "Caloric content: 299 kcal.\nDegree 40\nProteins: 0 g.\nFats: 0 g.\nCarbohydrates: 40 g.",
+                'description': "Caloric content: 299 kcal Degree 40 Proteins: 0g Fats: 0g Carbohydrates: 40g",
                 'expiration': '1 year',
-                'price': 27.99
+                'price': 27.99,
+                'barcode': '7777'
             },
             {
-                'image': '',
+                'image': 'images/marinated-tomatoes-with-garlic.jpg',
                 'name': 'Tomatoes',
                 'description': 'Tomatoes',
                 'expiration': '2 months',
-                'price': 8.99
+                'price': 8.99,
+                'barcode': '8888'
             },
             {
-                'image': '',
+                'image': 'images/Swiss_Cheese_Garden_of_Eden_jpg.jpg',
                 'name': 'Cheese',
-                'description': "Water - 29.16 g\nProteins - 35.75 g\nFats - 25.83 g\nCarbohydrates - 3.22 g\nAshes - 6.04 g",
+                'description': "Water - 29.16g Proteins - 35.75g Fats - 25.83g Carbohydrates - 3.22g Ashes - 6.04g",
                 'expiration': '2 months',
-                'price': 19.99
+                'price': 19.99,
+                'barcode': '9999'
             }
         ]
         self.setupUi(self)
@@ -113,6 +155,33 @@ class MyMainWindow(QMainWindow, selfcheckout_ui.Ui_MainWindow):
         self.proceedPushButton.clicked.connect(self.clickProceedButton)
         self.cartPushButton.clicked.connect(self.clickCartButton)
         self.getGoods()
+        self.discountImageLabel1.setPixmap(
+            QPixmap(self.discountGoods[0]['image']).scaled(self.discountImageLabel1.width(), self.discountImageLabel1.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel2.setPixmap(
+            QPixmap(self.discountGoods[1]['image']).scaled(self.discountImageLabel2.width(), self.discountImageLabel2.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel3.setPixmap(
+            QPixmap(self.discountGoods[2]['image']).scaled(self.discountImageLabel3.width(), self.discountImageLabel3.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel4.setPixmap(
+            QPixmap(self.discountGoods[3]['image']).scaled(self.discountImageLabel4.width(), self.discountImageLabel4.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel5.setPixmap(
+            QPixmap(self.discountGoods[4]['image']).scaled(self.discountImageLabel5.width(), self.discountImageLabel5.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel6.setPixmap(
+            QPixmap(self.discountGoods[5]['image']).scaled(self.discountImageLabel6.width(), self.discountImageLabel6.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel7.setPixmap(
+            QPixmap(self.discountGoods[6]['image']).scaled(self.discountImageLabel7.width(), self.discountImageLabel7.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel8.setPixmap(
+            QPixmap(self.discountGoods[7]['image']).scaled(self.discountImageLabel8.width(), self.discountImageLabel8.height(),
+                                                           Qt.KeepAspectRatio))
+        self.discountImageLabel9.setPixmap(
+            QPixmap(self.discountGoods[8]['image']).scaled(self.discountImageLabel9.width(), self.discountImageLabel9.height(),
+                                                           Qt.KeepAspectRatio))
         self.centralwidget.showFullScreen()
         # self.showFullScreen()
 
@@ -134,7 +203,6 @@ class MyMainWindow(QMainWindow, selfcheckout_ui.Ui_MainWindow):
         self.stackedWidget_3.setCurrentIndex(1)
 
     def clickFindButton(self):
-        self.setStyleSheet(open(style).read())
         if self.stackedWidget_2.currentIndex() == 0:
             self.stackedWidget_2.setCurrentIndex(1)
         else:
@@ -152,69 +220,70 @@ class MyMainWindow(QMainWindow, selfcheckout_ui.Ui_MainWindow):
         if selected_index >= 0:
             selected_item = self.goods[selected_index]
             self.nameGoodLabel.setText(selected_item['name'])
-            # self.descriptionGoodLabel.setText(selected_item['description'])
-            # self.expirationGoodLabel.setText('Until ' + selected_item['expiration'])
-            self.descriptionGoodLabel.setText(selected_item['name'])
-            self.expirationGoodLabel.setText(selected_item['name'])
-            self.imageGoodLabel.setPixmap(QPixmap('/home/tk/PycharmProjects/SelfCheckoutOnGo/app/static/upload/wares/1/1.jpg'))
+            self.descriptionGoodLabel.setText(selected_item['description'])
+            self.expirationGoodLabel.setText('Until ' + selected_item['expiration'])
+            img = QPixmap(selected_item['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio)
+            self.imageGoodLabel.setPixmap(img)
 
     def clickDiscountButton1(self):
         self.nameGoodLabel.setText(self.discountGoods[0]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[0]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[0]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel1.pixmap())
+        self.imageGoodLabel.setScaledContents(False)
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[0]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton2(self):
         self.nameGoodLabel.setText(self.discountGoods[1]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[1]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[1]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel2.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[1]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton3(self):
         self.nameGoodLabel.setText(self.discountGoods[2]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[2]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[2]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel3.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[2]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton4(self):
         self.nameGoodLabel.setText(self.discountGoods[3]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[3]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[3]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel4.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[3]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton5(self):
         self.nameGoodLabel.setText(self.discountGoods[4]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[4]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[4]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel5.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[4]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton6(self):
         self.nameGoodLabel.setText(self.discountGoods[5]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[5]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[5]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel6.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[5]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton7(self):
         self.nameGoodLabel.setText(self.discountGoods[6]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[6]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[6]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel7.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[6]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton8(self):
         self.nameGoodLabel.setText(self.discountGoods[7]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[7]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[7]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel8.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[7]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def clickDiscountButton9(self):
         self.nameGoodLabel.setText(self.discountGoods[8]['name'])
         self.descriptionGoodLabel.setText(self.discountGoods[8]['description'])
         self.expirationGoodLabel.setText(self.discountGoods[8]['expiration'])
-        self.imageGoodLabel.setPixmap(self.discountImageLabel9.pixmap())
+        self.imageGoodLabel.setPixmap(QPixmap(self.discountGoods[8]['image']).scaled(self.imageGoodLabel.width(), self.imageGoodLabel.height(), Qt.KeepAspectRatio))
 
     def getGoods(self):
         response = requests.get('http://40.68.188.63:10002/wares')
         self.all_goods = response.json()['wares']
+        self.all_goods = [t for t in self.discountGoods if True]
         self.goods = []
         print(self.all_goods)
 
@@ -229,14 +298,18 @@ class MyMainWindow(QMainWindow, selfcheckout_ui.Ui_MainWindow):
         selected = [t for t in self.all_goods if t['barcode'] == barcode]
         if len(selected) > 0:
             scanedGood = selected[0]
-            self.scannedGood = scanedGood
+            self.scannedDialog.scannedGood = scanedGood
             self.scannedDialog.scanedGoodNameLabel.setText(scanedGood['name'])
-            self.scannedDialog.scanedGoodPriceLabel.setText('price')
-            self.scannedDialog.scanedGoodDescriptionLabel.setText('description')
-            self.scannedDialog.scanedGoodExpirationLabel.setText('expiration')
-            self.scannedDialog.scanedGoodImageLabel.setPixmap(QPixmap('/home/tk/PycharmProjects/SelfCheckoutOnGo/app/static/upload/wares/1/1.jpg'))
+            self.scannedDialog.scanedGoodPriceLabel.setText(str(scanedGood['price']) + '€')
+            self.scannedDialog.scanedGoodDescriptionLabel.setText(scanedGood['description'])
+            self.scannedDialog.scanedGoodExpirationLabel.setText(scanedGood['expiration'])
+            self.scannedDialog.scanedGoodImageLabel.setPixmap(
+                QPixmap(scanedGood['image']).scaled(self.scannedDialog.scanedGoodImageLabel.width(),
+                                                               self.scannedDialog.scanedGoodImageLabel.height(),
+                                                               Qt.KeepAspectRatio))
             self.scannedDialog.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
             self.scannedDialog.move(400, 400)
+            self.scannedDialog.buttonsStackedWidget.setCurrentIndex(0)
             self.scannedDialog.show()
 
             # self.goods.append(selected[0])
